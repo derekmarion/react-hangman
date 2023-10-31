1efb8ac5554b1f66f3b2d5dd0d1850fc72f0b98f
Derek Marion <derek.j.marion@gmail.com>
2023-10-31 13:56:36 -0700

    Add functionality to render random word on the screen as underscores, or letters if guessed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import words from '../data/words.json'
 import './App.css'
+
+function Letter({letter, guessedLetters}) {
+  if (guessedLetters.includes(letter)) {
+    return <span>{letter}</span>;
+  } else {
+    return <span>_</span>;
+  }
+}
 
 function App() {
   const [puzzle, setPuzzle] = useState("");
@@ -9,12 +17,38 @@ function App() {
   const play = () => {
     const randInt = Math.floor(Math.random() * (words.length - 0));
     setPuzzle(words[randInt]);
-    console.log(puzzle);
   }
+
+  useEffect(() => {
+    console.log(puzzle);
+  }, [puzzle]);
+
+  const handleGuess = (letter) => {
+    setGuessedLetters([...guessedLetters, letter]);
+  };
+
+  const renderWord = () => {
+    return puzzle.split('').map((letter, index) => (
+      <Letter key={index} letter={letter} guessedLetters={guessedLetters} /> 
+    ));
+  };
 
   return (
     <>
       <button onClick={play}>Play Hangman</button>
+      <div>
+        {renderWord()}
+      </div>
+      <div>
+      <form
+              onClick={(e) => {
+                e.preventDefault();
+                handleGuess(letter);
+              }}
+            >
+          <input type="text" />
+      </form>
+      </div>
     </>
   )
 }
